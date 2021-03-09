b5811f0d05f588f9067664498228c4ac895ef8cc
pasi <pasi.vuohijoki@firstview.fi>
2021-03-09 12:16:43 +0200

    Added google analytics

--- app/app.js
+++ app/app.js
@@ -28,11 +28,12 @@ import '!file-loader?name=[name].[ext]!./images/favicon.ico';
 import 'file-loader?name=.htaccess!./.htaccess';
 /* eslint-enable import/no-unresolved, import/extensions */
 
+import ReactGA from 'react-ga';
 import configureStore from './configureStore';
-
 // Import i18n messages
 import { translationMessages } from './i18n';
-
+ReactGA.initialize('G-BELFP3JD0M');
+ReactGA.pageview(window.location.pathname + window.location.search);
 // Create redux store with history
 const initialState = {};
 const store = configureStore(initialState, history);
